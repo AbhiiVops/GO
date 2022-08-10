999157067543c882cc2672885c3ff827e7b61545
Abhishek bhattacharjee <abhiias2003@gmail.com>
2022-08-10 11:25:18 +0530

    first commit

---
+++ src/github.com/AbhiiVops/firstapp/Main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*
+		First way to initialise a variable
+		var i int
+		i = 42
+		i = 27
+	*/
+
+	// var i int = 42 --> second way of initialisation
+
+	// i := 42 --> Third way of initialisation
+	//fmt.Println(i)
+
+	var i int
+	i = 11
+
+	var j int = 05
+
+	k := 2003
+	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Println(k)
+
+}
